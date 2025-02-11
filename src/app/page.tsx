6bae5777f1815091063f9af39b186aeabf7c592c
Rajveer Singh <rajveersinghvns2021@gmail.com>
2025-02-11 23:26:58 +0530

    resolved bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Portfolio from "@/components/Portfolio";
 import Skills from "@/components/Skills";
-import Image from "next/image";
 
 export default function Home() {
   return (
